4c3aa8894b200567d87f1875b2e3dc3ba11bb876
Lucas Lopes de Oliveira <81447563+Lucas-Olv@users.noreply.github.com>
2021-08-13 15:59:39 -0300

    v0.2.0
    Adicionado calculo de juros simples.

--- Calculadora_de_Juros.dart
+++ Calculadora_de_Juros.dart
@@ -6,11 +6,12 @@ telaInicial();
 return  0;
 }
 
+//Função de boas vindas e apresentação do menu
 void telaInicial() {
   var selecionarMenu = 0;
   var selecionarMenuInput = "";
 
-  print("Seja bem vindo a calculadora de juros! v0.1.0");
+  print("Seja bem vindo a calculadora de juros! v0.2.0");
   print("1. Calcular juros simples.");
   print("2. Calcular juros compostos.");
 
@@ -38,7 +39,33 @@ void telaInicial() {
 }
 
 void calcJurosSimples() {
-  print("Hello");
+  
+  var juroSimplesInput = "";
+  double montante = 0;
+  double capitalInicial = 0;
+  double taxaJuros = 0;
+  double tempo = 0; 
+
+  print("Calculadora de juros simples selecionada");
+
+  stdout.write("Digite o valor inicial: ");
+  juroSimplesInput = stdin.readLineSync()!;
+  capitalInicial = double.parse(juroSimplesInput);
+
+  stdout.write("Digite a taxa de juros ao ano: ");
+  juroSimplesInput = stdin.readLineSync()!;
+  taxaJuros = double.parse(juroSimplesInput);
+
+  stdout.write("Digite o tempo em anos: ");
+  juroSimplesInput = stdin.readLineSync()!;
+  tempo= double.parse(juroSimplesInput);
+
+  taxaJuros = taxaJuros / 100;
+  montante = capitalInicial  + (capitalInicial * taxaJuros * tempo);
+
+print("O montante será de R\$$montante reais ao fim de $tempo anos e os juros acumulados serão de R\$${montante - capitalInicial} reais.");
+
+
 }
 
 void calcJurosComposto() {
